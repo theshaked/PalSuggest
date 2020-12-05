d23fff68a10b55c95cca9bda29688930a1ad1b5f
Shaked Sibony <Shaked2009@gmail.com>
2020-12-05 21:51:56 +0200

    Tag Spinner added to "Add Product window" with few tags.

--- app/src/main/java/com/example/palsuggest/activity_add_product.java
+++ app/src/main/java/com/example/palsuggest/activity_add_product.java
@@ -3,12 +3,32 @@ package com.example.palsuggest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class activity_add_product extends AppCompatActivity {
+public class activity_add_product extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        Spinner spinnerTags =findViewById(R.id.prodTag);
+        ArrayAdapter<CharSequence> adapterTags=ArrayAdapter.createFromResource(this,R.array.ProductTags, android.R.layout.simple_spinner_item);
+        adapterTags.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTags.setAdapter(adapterTags);
+        spinnerTags.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text=parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
