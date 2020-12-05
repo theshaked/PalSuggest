ce4e361e92c068de9df0083f1e5157a24e22ef68
Shaked SIbony <Shaked2009@gmail.com>
2020-12-05 17:21:06 +0200

    New "Add Product" Activity Added Without any backend logic.

--- app/src/main/java/com/example/palsuggest/MainActivity.java
+++ app/src/main/java/com/example/palsuggest/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.palsuggest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActBtnAddProd = findViewById(R.id.addProdFloatingActionButton);
+        floatingActBtnAddProd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "+ was Clicked", Toast.LENGTH_LONG).show(); //TODO: del this toast
+                openActivity(activity_add_product.class);
+            }
+        });
+
+
+    }
+
+    private void openActivity(Class activityClass)
+    {
+        startActivity(new Intent(this,activityClass));
     }
 }
